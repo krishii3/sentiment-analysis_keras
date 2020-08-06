0da1f00334858e956d567d5cf3aa242911f4b1d2
krishii3 <mridulkrishn3@gmail.com>
2020-08-07 01:02:40 +0530

    updatded

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from tensorflow.keras.models import load_model
 # from PIL import Image
 #except ImportError:
 # import Image
-#import pickles
+#import pickle
 
 ##############################################################################
 #pre-requisites variables and other essentials.
